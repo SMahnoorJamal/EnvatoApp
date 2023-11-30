c42988dca89621ecea2f31259751d76a5300320a
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2023-11-30 13:15:49 +0500

    home page

--- src/classes/HomePage/index.js
+++ src/classes/HomePage/index.js
@@ -1,17 +1,24 @@
-import {View, Text, } from 'react-native';
+import {View, Text, FlatList, } from 'react-native';
 import React from 'react';
 import Input from '../../components/Input'
+import Boxes from '../../components/Boxes';
 // import Button from '../../components/Button';
 
 
-function HomePage() {   
+ renderItem =() =>{
+
+    return(
+        // <View><Text>jjjjjjjjh</Text></View>
+        <Boxes/>
+    )
+}
+const  HomePage =() =>{   
     
         return(
             <View>
-                <View>
+                <View style={{width: '90%', marginLeft: 21}}>
                   <Text> Welcome, Toyln</Text> 
-
-                    <Input></Input>
+                    <Input style={{heigt: 100 , width: '110%'}}></Input>
 
                     <View style={{flexDirection: 'row'}}>
                     {/* <Button onPress={() }></Button>
@@ -19,6 +26,55 @@ function HomePage() {
                     <Button></Button> */}
                     </View>
                     
+
+                    <Text>What's Trending?</Text>
+                    <View style={{width: '100%', 
+                  marginTop: 10,  height: '126%'}}>
+                    
+                     <View>
+                     <FlatList  
+                    data={[  
+                        {key: 'Android'},{key: 'iOS'}, {key: 'Java'},{key: 'Swift'},  
+                        {key: 'Php'},{key: 'Hadoop'},{key: 'Sap'},  
+                        {key: 'Python'},{key: 'Ajax'}, {key: 'C++'},  
+                        {key: 'Ruby'},{key: 'Rails'},{key: '.Net'},  
+                        {key: 'Perl'},{key: 'Sap'},{key: 'Python'},  
+                        {key: 'Ajax'}, {key: 'C++'},{key: 'Ruby'},  
+                        {key: 'Rails'},{key: '.Net'},{key: 'Perl'}  
+                    ]}  
+                    renderItem={({item}) => 
+                    <View>
+                        
+                        <Boxes text={item.key}/>
+                    </View>
+                    }
+                    horizontal
+                    ItemSeparatorComponent={this.renderSeparator}/>
+                
+                  <FlatList  
+                    data={[  
+                        {key: 'Android'},{key: 'iOS'}, {key: 'Java'},{key: 'Swift'},  
+                        {key: 'Php'},{key: 'Hadoop'},{key: 'Sap'},  
+                        {key: 'Python'},{key: 'Ajax'}, {key: 'C++'},  
+                        {key: 'Ruby'},{key: 'Rails'},{key: '.Net'},  
+                        {key: 'Perl'},{key: 'Sap'},{key: 'Python'},  
+                        {key: 'Ajax'}, {key: 'C++'},{key: 'Ruby'},  
+                        {key: 'Rails'},{key: '.Net'},{key: 'Perl'}  
+                    ]}  
+                    renderItem={({item}) => 
+                    <View>
+                        
+                        <Boxes text={item.key}/>
+                    </View>
+                    }
+                    horizontal
+                    ItemSeparatorComponent={this.renderSeparator}/>
+                
+                     </View>
+                     
+
+                    </View>
+                   
                 </View>
             </View>
         )
